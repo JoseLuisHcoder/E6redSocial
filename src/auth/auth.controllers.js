fbd7007e0b5e8a12f267d3a167e985aa3b32f5c6
JoseLuisHcoder <zonajl@gmail.com>
2022-12-25 10:45:18 -0400

    update redSocial

--- src/auth/auth.controllers.js
+++ src/auth/auth.controllers.js
@@ -52,6 +52,7 @@ const changePassword = async (tokenId, newPassword) => {
     } 
 } 
 
+
 //? https://ejemplo.com/api/v1/auth/recovery-password/s6df51s3ad1f3sad5f43sd54f3sd54f3sad2f13sad4f
 
 module.exports = {
